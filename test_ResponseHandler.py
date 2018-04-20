5ef09f0e14eec90fb32c4045b061dac33960040c
Tomekske <joostenstomek@gmail.com>
2018-04-20 19:46:43 +0200

    log: 2
    Updated:
    - Documented unit test

--- test_ResponseHandler.py
+++ test_ResponseHandler.py
@@ -1,16 +1,17 @@
 #===============================================================================#
-#Title           :test_blue                                                     #
-#Description     :Unit test to test virtual assistant code                      #
+#Title           :Unit test ResponseHandler                                     #
+#Description     :Unit test to test Response class                              #
 #Author          :joostenstomek@gmail.com                                       #
-#Date            :16/04/2018                                                    #
+#Date            :20/04/2018                                                    #
 #Version         :1.0.0                                                         #
 #Usage           :Python                                                        #
 #Python version  :3.6                                                           #
 #===============================================================================#
 
+
+
 import unittest
 from ResponseHandler import Response
-
 
 
 
@@ -24,7 +25,9 @@ class TestCoreFunctions(unittest.TestCase):
 
 
 
-
+	##
+	## @brief     Method to determintate wether url is valid or invallid
+	##
 	def test_url(self):
 		r_valid = Response(self.url_valid)
 		r_invalid = Response(self.url_invalid)
@@ -32,19 +35,19 @@ class TestCoreFunctions(unittest.TestCase):
 		self.assertTrue(Response(r_valid.status))
 		self.assertTrue(Response(r_invalid.status))
 
+
+
+	##
+	## @brief     Method to determintate wether querry is valid or invallid
+	##
 	def test_querry(self):
 		r_valid = Response(self.url_valid)
 		r_invalid = Response(self.url_invalid)
 
 		self.assertNotEqual(r_valid.json, 'RAW_QUERRY_ERROR') #Querry is valid
 		self.assertEqual(r_invalid.json, 'RAW_QUERRY_ERROR') #Querry is invalid
+
 	
-
-
-
-
-		
-
 
 if __name__ == '__main__':
 	unittest.main()
